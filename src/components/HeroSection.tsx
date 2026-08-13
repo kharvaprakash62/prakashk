b906f3191ecce5a43b54aee7c7f35a1e40da06b0
Prakash Kharva <kharvaprakash62@gmail.com>
2026-08-13 19:00:54 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,7 @@
 import { ArrowDown, Briefcase, Monitor, Smartphone } from "lucide-react";
 
 // const profileImg = "https://i.postimg.cc/25HzBq1D/Image-(10).jpg";
-const profileImg = "src/assets/pkpro2.png";
+const profileImg = "assets/pkpro2.png";
 
 const HeroSection = () => {
   return (
